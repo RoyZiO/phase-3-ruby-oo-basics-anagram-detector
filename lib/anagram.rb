375aa5f10bd210e34a808d395c8c4d3c6ebd4cdb
RoyZiO <nziokinroy@gmail.com>
2023-02-13 02:57:33 +0300

    Completed bonus lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :match
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(elements)
+        elements.select do |element|
+        element.chars.sort == @word.chars.sort
+         end
+    end
+end
+
+anagram = Anagram.new("listen")
+puts anagram.match(%w(enlists google inlets banana))
